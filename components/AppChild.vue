a9be1625dd16da734735ea254bf3ec2812b20e66
Kass <56520362+kassandraaubin@users.noreply.github.com>
2022-03-02 14:47:51 +0100

    And so, it begins.

---
+++ components/AppChild.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="item">
+    <h2>{{item}}</h2>
+    <img :src="url" width="235" height="300" alt="">
+    <div class="quantity">
+        <button class="inc" @click="counter > 0 ? decrementing() : 0">-</button>
+        <span>Quantity: {{counter}}</span>
+        <button class="inc" @click="incrementing()">+</button>
+    </div>
+    <button class="submit">Submit</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  props: ['item', 'url'],
+  methods: {
+    decrementing () {
+      this.counter -= 1;
+    },
+    incrementing () {
+      this.counter += 1;
+    }
+  }
+}
+</script>
+
---
+++ pages/cart.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="cartunit">
+        <h2>Your cart</h2>
+        <p>Thanks for shopping with us.</p>
+        <div v-if="cart"></div>
+        <div v-else>
+            <p>
+                Your cart is empty, why not <nuxt-link to="/">
+                    fill it up with something ?
+                </nuxt-link>
+
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            cart: ""
+        }
+    }
+}
+</script>
+
+<style scoped>
+.cartunit {
+  padding-top: 120px;
+  width: 600px;
+  padding: 120px 0 40px;
+  max-width: 900px;
+  margin: 0 auto;
+}
+h2 {
+  margin-bottom: 40px;
+}
+</style>
